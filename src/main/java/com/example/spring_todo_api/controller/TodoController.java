c7bb24bf844ee585c3c853336fe2a874c2781d50
Andika Pradnyana <andikapradnyana16@gmail.com>
2025-11-14 17:47:38 +0800

    feat: adding todo service and it's method

--- src/main/java/com/example/spring_todo_api/controller/TodoController.java
+++ src/main/java/com/example/spring_todo_api/controller/TodoController.java
@@ -1,4 +1,7 @@
 package com.example.spring_todo_api.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TodoController {
 }
--- src/main/java/com/example/spring_todo_api/service/TodoService.java
+++ src/main/java/com/example/spring_todo_api/service/TodoService.java
@@ -1,11 +1,39 @@
 package com.example.spring_todo_api.service;
 
+import com.example.spring_todo_api.entity.Todo;
+import com.example.spring_todo_api.entity.User;
+import com.example.spring_todo_api.model.CreateTodoRequest;
+import com.example.spring_todo_api.repository.TodoRepository;
+import com.example.spring_todo_api.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class TodoService {
 
+    @Autowired
+    private TodoRepository todoRepository;
 
-    
+    @Autowired
+    private ValidationService validator;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public void create(Integer userId, CreateTodoRequest data) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        Todo todo = new Todo();
+
+        todo.setUser(user);
+        todo.setTitle(data.getTitle());
+        todo.setDescription(data.getDescription());
+
+        todoRepository.save(todo);
+    }
 
 }
