3b24ea8d08c69da03a6457186f3eddd2508f9a68
Andika Pradnyana <andikapradnyana16@gmail.com>
2025-11-14 18:03:04 +0800

    feat: adding method existsByTitle to todoRepository

--- src/main/java/com/example/spring_todo_api/repository/TodoRepository.java
+++ src/main/java/com/example/spring_todo_api/repository/TodoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+    public boolean existsByTitle(String title);
 }
