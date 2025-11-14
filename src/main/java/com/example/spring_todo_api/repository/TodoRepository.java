69ee86851c0d4f090020f412cbd7e759351829e0
Andika Pradnyana <andikapradnyana16@gmail.com>
2025-11-14 17:46:26 +0800

    feat: adding repository for todo

--- src/main/java/com/example/spring_todo_api/repository/TodoRepository.java
+++ src/main/java/com/example/spring_todo_api/repository/TodoRepository.java
@@ -1,4 +1,9 @@
 package com.example.spring_todo_api.repository;
 
-public class TodoRepository {
+import com.example.spring_todo_api.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
 }
