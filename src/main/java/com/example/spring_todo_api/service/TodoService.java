75ff009b7c41493f948938027d69c74fa0276d4d
Andika Pradnyana <andikapradnyana16@gmail.com>
2025-11-15 18:29:15 +0800

    feat: completing all the logic in todo service and adding response object for todo itself

--- src/main/java/com/example/spring_todo_api/service/TodoService.java
+++ src/main/java/com/example/spring_todo_api/service/TodoService.java
@@ -3,9 +3,13 @@ package com.example.spring_todo_api.service;
 import com.example.spring_todo_api.entity.Todo;
 import com.example.spring_todo_api.entity.User;
 import com.example.spring_todo_api.model.CreateTodoRequest;
+import com.example.spring_todo_api.model.TodoResponse;
+import com.example.spring_todo_api.model.UpdateTodoRequest;
 import com.example.spring_todo_api.repository.TodoRepository;
 import com.example.spring_todo_api.repository.UserRepository;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -29,22 +33,20 @@ public class TodoService {
         validator.validate(data);
 
         User user = userRepository
-            .findById(userId)
-            .orElseThrow(() ->
-                new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    "User not found"
-                )
-            );
-
-
+                .findById(userId)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "User not found"
+                        )
+                );
 
         boolean existingTodo = todoRepository.existsByTitle(data.getTitle());
 
         if (existingTodo) {
             throw new ResponseStatusException(
-                HttpStatus.BAD_REQUEST,
-                "Todo already exists"
+                    HttpStatus.BAD_REQUEST,
+                    "Todo already exists"
             );
         }
 
@@ -62,11 +64,51 @@ public class TodoService {
 
         if (todos.isEmpty()) {
             throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND,
-                "No todos found"
+                    HttpStatus.NOT_FOUND,
+                    "No todos found"
             );
         }
 
         return todos;
     }
+
+    public TodoResponse getFromUser(Integer userId) {
+        Todo todo = todoRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No todo found"));
+
+        return TodoResponse.builder().title(todo.getTitle()).description(todo.getDescription()).completed(todo.getCompleted()).build();
+    }
+
+    @Transactional
+    public TodoResponse updateTodo(Integer userId, UpdateTodoRequest data) {
+        validator.validate(data);
+
+        User user =  userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        boolean exists = todoRepository.existsByTitle(data.getTitle());
+
+        if(!exists) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found");
+        }
+
+        Todo todo = new Todo();
+
+        todo.setTitle(data.getTitle());
+        todo.setDescription(data.getDescription());
+        todo.setCompleted(data.getCompleted());
+
+        todoRepository.save(todo);
+
+        return TodoResponse.builder().title(todo.getTitle()).description(todo.getDescription()).completed(todo.getCompleted()).build();
+    }
+
+    public void delete(Integer todoId,  Integer userId) {
+        Todo todo = todoRepository.findById(todoId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found"));
+
+        if(todo.getUser().getId().equals(userId)) {
+            todoRepository.delete(todo);
+        } else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You are not allowed to delete this todo");
+        }
+    }
+
 }
